12628bbea31b3a0cff6a8b6fcfbbea6b5a838e31
JulienNirxo <millaud.julien@gmail.com>
2023-12-19 14:59:45 +0100

    fix delete button check

--- speed_video/content.js
+++ speed_video/content.js
@@ -1,13 +1,18 @@
-// Fonction pour contrôler la vitesse de la vidéo
-function controlVideoSpeed(speed) {
-    const videos = document.querySelectorAll('video');
-    videos.forEach(video => {
-        video.playbackRate = speed;
+speedValue = 1.0;
+
+//if step in input change speed
+document.addEventListener('DOMContentLoaded', function () {
+    const speedInput = document.getElementById('speedInput');
+    speedInput.addEventListener('change', function () {
+        const speedValue = speedInput.value;
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { action: 'changeSpeed', speed: speedValue });
+        });
     });
-}
+});
 
 // Envoie un message à l'extension pour changer la vitesse de la vidéo
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (request) {
     if (request.action === 'changeSpeed') {
         const speed = parseFloat(request.speed);
         if (!isNaN(speed)) {
@@ -15,3 +20,11 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         }
     }
 });
+
+// Fonction pour contrôler la vitesse de la vidéo
+function controlVideoSpeed(speedValue) {
+    const videos = document.querySelectorAll('video');
+    videos.forEach(video => {
+        video.playbackRate = speedValue;
+    });
+}
